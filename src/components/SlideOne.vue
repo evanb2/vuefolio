76355c2d8c3bdcd02a9af4743b806020abe90b93
Evan Butler <evanbutler@FM-evabut-Mac.local>
2016-11-17 20:49:38 -0800

    adds SlideOne and SlideTwo components

---
+++ src/components/SlideOne.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img class="slide__background"
+         src="../assets/img/slide-one-bg.jpg"
+         alt="carpenters tools">
+    <h1 class="slide__title">Web Architect</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  };
+</script>
+
+<style lang="scss">
+  h1 {
+    float: right;
+  }
+</style>
